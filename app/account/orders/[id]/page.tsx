50fb1a80ff1b10bf838fa2e965a3c718bda1ec43
Houssam NINIA <niniahoussam69@gmail.com>
2026-09-20 11:56:05 +0100

    Show collection-only order details

--- app/account/orders/[id]/page.tsx
+++ app/account/orders/[id]/page.tsx
@@ -184,17 +184,9 @@ export default function AccountOrderPage() {
 
           <dl>
             <div>
-              <dt>Receive by</dt>
-              <dd>
-                {order.fulfillment === 'delivery' ? 'Delivery' : 'Collection'}
-              </dd>
+              <dt>Collection</dt>
+              <dd>ENSAM Rabat</dd>
             </div>
-            {order.address && (
-              <div>
-                <dt>Address</dt>
-                <dd>{order.address}</dd>
-              </div>
-            )}
             {order.notes && (
               <div>
                 <dt>Your note</dt>
@@ -205,12 +197,6 @@ export default function AccountOrderPage() {
               <dt>Subtotal</dt>
               <dd>{order.subtotal} MAD</dd>
             </div>
-            {order.delivery_fee > 0 && (
-              <div>
-                <dt>Delivery fee</dt>
-                <dd>{order.delivery_fee} MAD</dd>
-              </div>
-            )}
             <div>
               <dt>Payment</dt>
               <dd>{order.payment_status === 'paid' ? 'Paid' : 'Unpaid'}</dd>
